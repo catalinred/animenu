e8408ae0d9b7c745b77d825488ebcf5a0406106f
Catalin Rosu <redmaniack@gmail.com>
2014-06-05 16:42:05 +0300

    - Toggle button updated - classList polyfill added - added attachEvent as fallback for addEventListener (IE) - added respond.js for IE8 responsiveness

--- js/animenu.js
+++ js/animenu.js
@@ -1,11 +1,15 @@
 (function(){
-  var animenuToggle       = document.querySelector('.animenu__toggle'),
-      animenuNav          = document.querySelector('.animenu__nav'),
-      animenuBreakpoint   = 767;
+  var animenuToggle = document.querySelector('.animenu__toggle'),
+      animenuNav    = document.querySelector('.animenu__nav'),
+      animenuToggleNav =  function (){        
+        animenuToggle.classList.toggle("animenu__toggle--active");
+        animenuNav.classList.toggle("animenu__nav--open");        
+      }
 
-  animenuToggle.addEventListener('click', function(){
-    if(window.innerWidth < animenuBreakpoint){
-        animenuNav.classList.toggle("animenu__nav--open");
-    }
-  });	
+  if (!animenuToggle.addEventListener) {
+      animenuToggle.attachEvent("onclick", animenuToggleNav);
+  }
+  else {
+      animenuToggle.addEventListener('click', animenuToggleNav);
+  }
 })()
