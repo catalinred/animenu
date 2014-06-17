93c943068dcbd7f6caad1fa8ec782d6db187bb07
Catalin Rosu <redmaniack@gmail.com>
2014-06-17 23:38:22 +0300

    - Remove the classList polyfill and add toggleClass function instead. - HTML5 shim only for the nav element (ie8)

--- js/animenu.js
+++ js/animenu.js
@@ -1,9 +1,23 @@
 (function(){
   var animenuToggle = document.querySelector('.animenu__toggle'),
       animenuNav    = document.querySelector('.animenu__nav'),
+      hasClass = function( elem, className ) {
+        return new RegExp( ' ' + className + ' ' ).test( ' ' + elem.className + ' ' );
+      },
+      toggleClass = function( elem, className ) {
+        var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ' ) + ' ';
+        if( hasClass(elem, className ) ) {
+          while( newClass.indexOf( ' ' + className + ' ' ) >= 0 ) {
+            newClass = newClass.replace( ' ' + className + ' ' , ' ' );
+          }
+          elem.className = newClass.replace( /^\s+|\s+$/g, '' );
+        } else {
+          elem.className += ' ' + className;
+        }
+      },           
       animenuToggleNav =  function (){        
-        animenuToggle.classList.toggle("animenu__toggle--active");
-        animenuNav.classList.toggle("animenu__nav--open");        
+        toggleClass(animenuToggle, "animenu__toggle--active");
+        toggleClass(animenuNav, "animenu__nav--open");        
       }
 
   if (!animenuToggle.addEventListener) {
